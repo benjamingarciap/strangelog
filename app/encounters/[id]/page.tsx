7dccc13edf5ca161c5bf6542d500025a15ff971b
Benjamin Garcia <benjamingarcia@Benjamins-MBP.home>
2025-09-05 11:44:10 +0200

    fix: params type error on vercel deployment

--- app/encounters/[id]/page.tsx
+++ app/encounters/[id]/page.tsx
@@ -4,7 +4,8 @@ import { fetchEncounterById } from '@/app/lib/data'
 import EncounterDetail from '../../ui/encounters/encounter-detail'
 
 // This is your dynamic route page
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params
   const { id } = params
   // console.log('Fetching encounter with ID:', id)
   const encounter = await fetchEncounterById(id)
