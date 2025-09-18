35fdaaf1aef53bf6567016865de40850028f61d0
Benjamin Garcia <benjamingarcia@Benjamins-MBP.home>
2025-09-18 21:28:43 +0200

    chore(ui): clean up MapWrapper imports and component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-// import Encounters from './encounters/page'
-// import MapWrapper from './ui/map/mapWrapper'
 import { fetchEncounters } from './lib/data-encounters'
 import React from 'react'
 import { fetchUserById } from './lib/data-users'
@@ -10,13 +8,4 @@ export default async function Home(): Promise<React.ReactElement> {
   const user = await fetchUserById(121)
   console.log('Fetched user from DB:', user)
   return <Dashboard encounters={encounters} />
-  // <div className="flex flex-row justify-around gap-4 w-full h-screen">
-  // <div className="grid grid-cols-1 sm:grid-cols-6 md:grid-cols-6">
-  //   <div className="overflow-y-auto col-span-3 h-full">
-  //     <MapWrapper encounters={encounters} />
-  //   </div>
-  //   <div className="overflow-y-auto col-span-3 h-full">
-  //     <Encounters />
-  //   </div>
-  // </div>
 }
